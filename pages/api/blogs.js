d99d3bec3957551b3db7e2a9d7ae7ad3543766f4
faixan554 <faixan.malik554@gmail.com>
2022-12-07 21:12:04 +0500

    refactring in blog.js

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,10 +1,25 @@
 import * as fs from 'fs';
 
-export default function handler(req, res) {
-  fs.readdir('blogdata', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    res.status(200).json(data)
-  });
+export default async function handler(req, res) {
+    let data = await fs.promises.readdir("blogdata")
+    res.status(200).json(data);
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  // fs.readdir('blogdata', function (err,data) {
+  //   if (err) {
+  //     return console.log(err);
+  //   }
+  //   res.status(200).json(data)
+  // });
 }
