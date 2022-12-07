4ed6ac98caa6237cf653c3256ca3c050104ccb3d
faixan554 <faixan.malik554@gmail.com>
2022-12-07 15:09:24 +0500

    add blogsdata in form of jsons

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -3,13 +3,13 @@ import Link from 'next/link'
 
 function Navbar() {
   return (
-    <header class="text-gray-600 body-font">
+    <header class="text-gray-600 body-font border-b-2 ">
         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
             <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                 <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
-            <span class="ml-3 text-xl">Hunting_Coder</span>
+            <span class="ml-3 text-2xl font-bold text-blue-800">Hunting_Coder</span>
             </a>
             <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-xl justify-center list-none space-x-8">
             <Link href={"/"}><li>Home</li></Link> 
@@ -17,11 +17,7 @@ function Navbar() {
             <Link href={"/about"}><li>About</li></Link>
             <Link href={"/contact"}><li>Contact</li></Link>
             </nav>
-            <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Sign Up
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-            </button>
+            <button class="inline-flex items-center bg-blue-700 text-white rounded-xl font-semibold border-0 py-2 px-5 focus:outline-none hover:bg-blue-800 text-base mt-4 md:mt-0">Sign Up</button>
         </div>
     </header>
   )
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -5,7 +5,15 @@ function slug() {
     const router = useRouter();
     const {slug} = router.query;
   return (
-    <div className='text-center text-4xl pt-10 text-blue-600 font-bold'>{slug}</div>
+    <div>
+      <div>
+        <h1 className='text-center text-4xl pt-10 text-blue-600 font-bold'>Title of the page is {slug}</h1>
+        <div className='container my-8 w-1/2 mx-auto text-center'>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum impedit architecto voluptatibus saepe ducimus totam aperiam, ex qui a rem ipsam similique nihil consectetur fugiat debitis id deleniti nobis obcaecati earum quod. Tenetur ipsum beatae reiciendis fugit, eaque sequi repudiandae fugiat ex tempora delectus, aperiam illo quod! Voluptatum, facere est.</p>
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
