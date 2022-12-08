a8faf29b543afdac1fd1d64942a3116c9c98dff7
faixan554 <faixan.malik554@gmail.com>
2022-12-08 13:22:09 +0500

    blogs.js endpoint ready

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -2,24 +2,16 @@ import * as fs from 'fs';
 
 export default async function handler(req, res) {
     let data = await fs.promises.readdir("blogdata")
-    res.status(200).json(data);
-
-
-
-
-
-
-
-
-
-
-
-
+  
+    // itterate the data from the directory using for loop
+    let myfile;
+    let alldata=[];
+    for (let i = 0; i < data.length; i++) {
+        let file = data[i];
+        let myfile = await fs.promises.readFile((`blogdata/${file}`),('utf-8'));
+        alldata.push(JSON.parse(myfile));
+    }   
+    res.status(200).json(alldata); 
+   
     
-  // fs.readdir('blogdata', function (err,data) {
-  //   if (err) {
-  //     return console.log(err);
-  //   }
-  //   res.status(200).json(data)
-  // });
-}
+  }
