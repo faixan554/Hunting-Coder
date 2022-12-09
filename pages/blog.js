b92d67b78e7f6594034aa15826e4d12309758a7f
faixan554 <faixan.malik554@gmail.com>
2022-12-09 11:12:25 +0500

    set state in blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -4,28 +4,15 @@ import Link from 'next/link'
 // fetch data using useEffect
 function Blog() {
 
-
   const [blogdata, setBlogdata] = useState([])
    useEffect(() => {
-    fetch('http://localhost:3000/api/blogs').then((a) => a.json())
+    fetch('http://localhost:3000/api/blogs').then((res) => res.json())
      .then((data) => {
         setBlogdata(data);
         console.log(data);
       })
     },[])
-
-
-
-
-
-
-
-
- 
-
-
-
-
+    
 
   return (
     <div className='text-center pt-16 space-y-5 text-lg pb-20'>
@@ -44,6 +31,7 @@ function Blog() {
           <h1 className='text-2xl'>How to learn Javascript</h1>
           <p className='text-base'>learn Javascript in one video is the best way to learn Javascript</p>
         </div>
+
         
       
       </div>
