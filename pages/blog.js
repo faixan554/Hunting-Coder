7f29b456a5be1e9010cc02e9db56b476b651666f
faixan554 <faixan.malik554@gmail.com>
2022-12-09 15:09:57 +0500

    add content in blog page from json file usin map

--- pages/blog.js
+++ pages/blog.js
@@ -13,28 +13,20 @@ function Blog() {
       })
     },[])
 
-
-
-    
-
   return (
     <div className='text-center pt-16 space-y-5 text-lg pb-20'>
-      <h1 className='text-6xl'>Latest Blogs</h1>
-        <div>
-          <Link href={'/blogpost/learn-javascript'}>
-              <h1 className='text-2xl cursor-pointer'>How to learn Javascript</h1>
-          </Link>
-          <p className='text-base'>learn Javascript in one video is the best way to learn Javascript</p>
-        </div>
-        <div>
-          <h1 className='text-2xl'>How to learn Javascript</h1>
-          <p className='text-base'>learn Javascript in one video is the best way to learn Javascript</p>
-        </div>
-        
-
-        
       
-      </div>
+      <h1 className='text-6xl'>Latest Blogs</h1>
+        {blogdata.map((blogitem)=>{
+          return <div key={blogitem.title}>
+            <Link href={'/blogpost/learn-javascript'}>
+                <h1 className='text-2xl cursor-pointer'>{blogitem.title}</h1>
+            </Link>
+            <p className='text-base'>learn Javascript in one video is the best way to learn Javascript</p>
+          </div>
+        })}
+
+    </div>
 
   )
 }
