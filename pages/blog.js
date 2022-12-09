d9bd5777c1e3e1acb284ceb117171aac81e91ff5
faixan554 <faixan.malik554@gmail.com>
2022-12-09 17:26:00 +0500

    set blogpost page the link page inside blog

--- pages/blog.js
+++ pages/blog.js
@@ -6,23 +6,22 @@ function Blog() {
 
   const [blogdata, setBlogdata] = useState([])
    useEffect(() => {
-    fetch('http://localhost:3000/api/blogs').then((res) => res.json())
+    fetch('http://localhost:3000/api/blogs').then((a) => a.json())
      .then((data) => {
         setBlogdata(data);
-        console.log(data);
       })
-    },[])
+    })
 
   return (
     <div className='text-center pt-16 space-y-5 text-lg pb-20'>
       
-      <h1 className='text-6xl'>Latest Blogs</h1>
+      <h1 className='text-5xl pb-7 font-bold text-blue-800'>Latest Blogs</h1>
         {blogdata.map((blogitem)=>{
-          return <div key={blogitem.title}>
-            <Link href={'/blogpost/learn-javascript'}>
-                <h1 className='text-2xl cursor-pointer'>{blogitem.title}</h1>
+          return <div key={blogitem.slug}>
+            <Link href={`/blogpost/${blogitem.slug}`}>
+                <h1 className='text-2xl text-blue-800 font-semibold p-3 cursor-pointer'>{blogitem.title}</h1>
             </Link>
-            <p className='text-base'>learn Javascript in one video is the best way to learn Javascript</p>
+            <p className='text-base text-justify text-gray-600 w-1/2 mx-auto'>{blogitem.content.substr(0,250)}</p>
           </div>
         })}
 
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,18 +1,32 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
 import { useRouter } from 'next/router'
 
 function slug() {
-    const router = useRouter();
-    const {slug} = router.query;
+    
+  const router = useRouter();
+  
+  
+  const [blog, setBlog] = useState();
+    useEffect(() => {
+      if (!router.isReady) return;
+      const {slug} = router.query;
+      fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a) => {
+         return a.json();
+        })
+      .then((data) => {
+          setBlog(data);
+        })
+      },[router.isReady])
+    
+
   return (
     <div>
       <div>
-        <h1 className='text-center text-4xl pt-10 text-blue-600 font-bold'>Title of the page is {slug}</h1>
-        <div className='container my-8 w-1/2 mx-auto text-center'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum impedit architecto voluptatibus saepe ducimus totam aperiam, ex qui a rem ipsam similique nihil consectetur fugiat debitis id deleniti nobis obcaecati earum quod. Tenetur ipsum beatae reiciendis fugit, eaque sequi repudiandae fugiat ex tempora delectus, aperiam illo quod! Voluptatum, facere est.</p>
+        <h1 className='text-center text-4xl pt-10 text-blue-600 font-bold'>{blog && blog.title}</h1>
+        <div className='container my-8 w-3/4 mx-auto text-justify'>
+          <p>{blog && blog.content}</p>
         </div>
       </div>
-      
     </div>
   )
 }
