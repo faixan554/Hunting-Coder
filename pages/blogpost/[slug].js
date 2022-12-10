9dca5ff167ccf609829e135f3b79fc9fda369240
faixan554 <faixan.malik554@gmail.com>
2022-12-10 13:09:46 +0500

    use ssr instead of useEffect syntax

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,22 +1,11 @@
 import React , {useState, useEffect} from 'react'
 import { useRouter } from 'next/router'
 
-function slug() {
+function slug(props) {
     
   const router = useRouter();
-  
-  
-  const [blog, setBlog] = useState();
-    useEffect(() => {
-      if (!router.isReady) return;
-      const {slug} = router.query;
-      fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a) => {
-         return a.json();
-        })
-      .then((data) => {
-          setBlog(data);
-        })
-      },[router.isReady])
+  const [blog, setBlog] = useState(props.blog);
+    
     
 
   return (
@@ -30,5 +19,34 @@ function slug() {
     </div>
   )
 }
+
+
+// Server side rendering
+export async function getServerSideProps(context) {
+  // Fetch data from external API
+  const {slug} = context.query;
+  const res = await fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+  const blog = await res.json()
+
+
+  // useEffect(() => {
+  //     if (!router.isReady) return;
+  //     const {slug} = router.query;
+  //     fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a) => {
+  //        return a.json();
+  //       })
+  //     .then((data) => {
+  //         setBlog(data);
+  //       })
+  //     },[router.isReady])
+
+
+
+  // Pass data to the page via props
+  return { props: { blog } }
+}
+
+
+
 
 export default slug
