9b13f195d528ab85f46565102f27594b11aebf34
faixan554 <faixan.malik554@gmail.com>
2022-12-12 23:18:19 +0500

    trying to fixing 505 error

--- pages/api/getblog.js
+++ pages/api/getblog.js
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 export default function handler(req, res) {
 fs.readFile(`blogdata/${req.query.slug}.json`, 'utf8', function (err,data) {
     if (err) {
-        res.status(500).json({error:error})
+        res.status(500).json({error:err})
     }
     res.status(200).json(JSON.parse(data))
   });
